5582e67008ff91e69d3391b9e4d0dce8a2d2dae4
MeninoVini <78368944+MeninoVini@users.noreply.github.com>
2021-02-05 09:46:23 -0300

    PrimeiroTeste

---
+++ TiposPrimitivos.java
@@ -0,0 +1,21 @@
+
+package tiposprimitivos;
+
+import java.util.Scanner;
+
+public class TiposPrimitivos {
+ 
+   
+  
+    public static void main(String[] args) {
+         Scanner teclado= new Scanner(System.in);
+   System.out.print("Digite o seu nome: ");
+   String nome= teclado.nextLine();
+   System.out.print("Digite a sua nota: ");
+   float nota= teclado.nextFloat();
+   System.out.format("A nota de %s é %.1f/n" ,nome, nota);
+   
+
+    }
+    
+}
